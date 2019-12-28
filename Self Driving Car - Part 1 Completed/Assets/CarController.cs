4a3dcbd07b9470ba3143d8761db0e22cd56b77f2
AJTech2002 <bestbatchprogrammer101@gmail.com>
2019-12-29 02:04:57 +1000

    Seperate Parts

---
+++ Self Driving Car - Part 1 Completed/Assets/CarController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(NNet))]
+public class CarController : MonoBehaviour
+{
+    private Vector3 startPosition, startRotation;
+    private NNet network;
+
+    [Range(-1f,1f)]
+    public float a,t;
+
+    public float timeSinceStart = 0f;
+
+    [Header("Fitness")]
+    public float overallFitness;
+    public float distanceMultipler = 1.4f;
+    public float avgSpeedMultiplier = 0.2f;
+    public float sensorMultiplier = 0.1f;
+
+    [Header("Network Options")]
+    public int LAYERS = 1;
+    public int NEURONS = 10;
+
+    private Vector3 lastPosition;
+    private float totalDistanceTravelled;
+    private float avgSpeed;
+
+    private float aSensor,bSensor,cSensor;
+
+    private void Awake() {
+        startPosition = transform.position;
+        startRotation = transform.eulerAngles;
+        network = GetComponent<NNet>();
+
+        //TEST CODE
+        network.Initialise(LAYERS, NEURONS);
+    }
+
+    public void Reset() {
+
+        //TEST CODE
+        network.Initialise(LAYERS, NEURONS);
+
+        timeSinceStart = 0f;
+        totalDistanceTravelled = 0f;
+        avgSpeed = 0f;
+        lastPosition = startPosition;
+        overallFitness = 0f;
+        transform.position = startPosition;
+        transform.eulerAngles = startRotation;
+    }
+
+    private void OnCollisionEnter (Collision collision) {
+        Reset();
+    }
+
+    private void FixedUpdate() {
+
+        InputSensors();
+        lastPosition = transform.position;
+
+
+        (a, t) = network.RunNetwork(aSensor, bSensor, cSensor);
+
+
+        MoveCar(a,t);
+
+        timeSinceStart += Time.deltaTime;
+
+        CalculateFitness();
+
+        //a = 0;
+        //t = 0;
+
+
+    }
+
+
+    private void CalculateFitness() {
+
+        totalDistanceTravelled += Vector3.Distance(transform.position,lastPosition);
+        avgSpeed = totalDistanceTravelled/timeSinceStart;
+
+       overallFitness = (totalDistanceTravelled*distanceMultipler)+(avgSpeed*avgSpeedMultiplier)+(((aSensor+bSensor+cSensor)/3)*sensorMultiplier);
+
+        if (timeSinceStart > 20 && overallFitness < 40) {
+            Reset();
+        }
+
+        if (overallFitness >= 1000) {
+            //Saves network to a JSON
+            Reset();
+        }
+
+    }
+
+    private void InputSensors() {
+
+        Vector3 a = (transform.forward+transform.right);
+        Vector3 b = (transform.forward);
+        Vector3 c = (transform.forward-transform.right);
+
+        Ray r = new Ray(transform.position,a);
+        RaycastHit hit;
+
+        if (Physics.Raycast(r, out hit)) {
+            aSensor = hit.distance/20;
+            Debug.DrawLine(r.origin, hit.point, Color.red);
+        }
+
+        r.direction = b;
+
+        if (Physics.Raycast(r, out hit)) {
+            bSensor = hit.distance/20;
+            Debug.DrawLine(r.origin, hit.point, Color.red);
+        }
+
+        r.direction = c;
+
+        if (Physics.Raycast(r, out hit)) {
+            cSensor = hit.distance/20;
+            Debug.DrawLine(r.origin, hit.point, Color.red);
+        }
+
+    }
+
+    private Vector3 inp;
+    public void MoveCar (float v, float h) {
+        inp = Vector3.Lerp(Vector3.zero,new Vector3(0,0,v*11.4f),0.02f);
+        inp = transform.TransformDirection(inp);
+        transform.position += inp;
+
+        transform.eulerAngles += new Vector3(0, (h*90)*0.02f,0);
+    }
+
+}
---
+++ Self Driving Car - Part 1 Completed/Assets/NNet.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MathNet.Numerics.LinearAlgebra;
+using System;
+
+using Random = UnityEngine.Random;
+
+public class NNet : MonoBehaviour
+{
+    public Matrix<float> inputLayer = Matrix<float>.Build.Dense(1, 3);
+
+    public List<Matrix<float>> hiddenLayers = new List<Matrix<float>>();
+
+    public Matrix<float> outputLayer = Matrix<float>.Build.Dense(1, 2);
+
+    public List<Matrix<float>> weights = new List<Matrix<float>>();
+
+    public List<float> biases = new List<float>();
+
+    public float fitness;
+
+    public void Initialise (int hiddenLayerCount, int hiddenNeuronCount)
+    {
+
+        inputLayer.Clear();
+        hiddenLayers.Clear();
+        outputLayer.Clear();
+        weights.Clear();
+        biases.Clear();
+
+        for (int i = 0; i < hiddenLayerCount + 1; i++)
+        {
+
+            Matrix<float> f = Matrix<float>.Build.Dense(1, hiddenNeuronCount);
+
+            hiddenLayers.Add(f);
+
+            biases.Add(Random.Range(-1f, 1f));
+
+            //WEIGHTS
+            if (i == 0)
+            {
+                Matrix<float> inputToH1 = Matrix<float>.Build.Dense(3, hiddenNeuronCount);
+                weights.Add(inputToH1);
+            }
+
+            Matrix<float> HiddenToHidden = Matrix<float>.Build.Dense(hiddenNeuronCount, hiddenNeuronCount);
+            weights.Add(HiddenToHidden);
+
+        }
+
+        Matrix<float> OutputWeight = Matrix<float>.Build.Dense(hiddenNeuronCount, 2);
+        weights.Add(OutputWeight);
+        biases.Add(Random.Range(-1f, 1f));
+
+        RandomiseWeights();
+
+    }
+
+    public void RandomiseWeights()
+    {
+
+        for (int i = 0; i < weights.Count; i++)
+        {
+
+            for (int x = 0; x < weights[i].RowCount; x++)
+            {
+
+                for (int y = 0; y < weights[i].ColumnCount; y++)
+                {
+
+                    weights[i][x, y] = Random.Range(-1f, 1f);
+
+                }
+
+            }
+
+        }
+
+    }
+
+    public (float, float) RunNetwork (float a, float b, float c)
+    {
+        inputLayer[0, 0] = a;
+        inputLayer[0, 1] = b;
+        inputLayer[0, 2] = c;
+
+        inputLayer = inputLayer.PointwiseTanh();
+
+        hiddenLayers[0] = ((inputLayer * weights[0]) + biases[0]).PointwiseTanh();
+
+        for (int i = 1; i < hiddenLayers.Count; i++)
+        {
+            hiddenLayers[i] = ((hiddenLayers[i - 1] * weights[i]) + biases[i]).PointwiseTanh();
+        }
+
+        outputLayer = ((hiddenLayers[hiddenLayers.Count-1]*weights[weights.Count-1])+biases[biases.Count-1]).PointwiseTanh();
+
+        //First output is acceleration and second output is steering
+        return (Sigmoid(outputLayer[0,0]), (float)Math.Tanh(outputLayer[0,1]));
+    }
+
+    private float Sigmoid (float s)
+    {
+        return (1 / (1 + Mathf.Exp(-s)));
+    }
+
+}
